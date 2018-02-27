5759d1d8f618e7e0e6af45f4ef53440ee141f383
jaubut <jeremie.aubut@gmail.com>
2018-02-26 23:45:08 -0500

    adding an other key..
    refuse to generating dynamic routes..

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -2,7 +2,7 @@ const contentful = require('contentful')
 // use default environment config for convenience
 // these will be set via `env` property in nuxt.config.js
 const config = {
-  space: process.env.CTF_SPACE_ID,
+  space: "n7526iq5odzp",
   accessToken: "73674a8db1f484c852da934a7154ddcc2b549484925f0fef26eea2f693fe8a0b"
 }
 
